01bcdd080ce564404ed0fb6870fbdf990ab6c424
sayanaka <sayaka.nakatsuka@pasonatech.jp>
2019-01-08 17:35:14 +0900

    init

---
+++ types/tabulator-tables /index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for tabulator-tables v4.1.4
+// Project: http://tabulator.info/
+// Definitions by: Sayaka Nakatsuka <https://github.com/sayanaka>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace Tabulator {}
+
+export = Tabulator;
